6d6ce5ff969e7f79995915a53c8eb932a47d3bd3
ByeongJu-Jeon <skhu202114128@gmail.com>
2025-01-23 16:09:46 +0900

    페이지별로 구분 쉽게 분리

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,37 @@ import Likes from "./pages/좋아요/Likes";
 import CreateCourse from "./pages/코스제작/CreateCourse";
 import Calender from "./pages/캘린더/Calender";
 import SelectDay from "./pages/캘린더/SelectDay";
+import CourseResult from "./pages/코스제작/CourseResult";
+import CourseEdit from "./pages/코스제작/CourseEdit";
+import CourseOrder from "./pages/코스제작/CourseOrder";
+import CourseSave from "./pages/코스제작/CourseSave";
 
 function App() {
   return (
     <Router>
       <Routes>
+        /* 홈페이지 */
         <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="/LoginHome" element={<LoginHome />} />
+        /* 로그인 및 회원가입 */
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/LoginHome" element={<LoginHome />} />
+        /* 마이페이지 */
         <Route path="/MyPage" element={<MyPage />} />
         <Route path="/edit/:field" element={<EditPage />} />
+        /* 좋아요 */
         <Route path="/Likes" element={<Likes />} />
-        <Route path="/CreateCourse" element={<CreateCourse />} />
+        /* 캘린더 */
         <Route path="/Calender" element={<Calender />} />
         <Route path="/SelectDay" element={<SelectDay />} />
+        /* 코스제작 */
+        <Route path="/CreateCourse" element={<CreateCourse />} />
+        <Route path="/CourseResult" element={<CourseResult />} />
+        <Route path="/CourseEdit" element={<CourseEdit />} />
+        <Route path="/CourseOrder" element={<CourseOrder />} />
+        <Route path="/CourseSave" element={<CourseSave />} />
+        /* 포스트 */
       </Routes>
     </Router>
   );
